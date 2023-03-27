1853e23d137dce9a5c1b8da2d29b01b6ef115e99
Arnab-108 <arnabadhikary007@gmail.com>
2023-03-27 22:43:39 +0530

    hello

--- src/component/My Stats.jsx
+++ src/component/My Stats.jsx
@@ -8,7 +8,7 @@ function Mystats(){
                 <Heading pt={"4vh"} fontWeight="semibold" textDecoration="underline" color={"white"}>My Statistics</Heading>
                 <VStack gap={"5vh"}>
                     <Flex mt="5vh" ml={"10vw"} align={"center"} >
-                        <Image  id="github-streak-stats"  width={"55vw"} height={"30vh"} src="https://github-readme-stats.vercel.app/api?username=Arnab-108" />
+                        <Image id="github-stats-card"    width={"55vw"} height={"30vh"} src="https://github-readme-stats.vercel.app/api?username=Arnab-108" />
                         <Image id="github-top-langs" pos={"relative"} right={"13vw"} width={"40vw"} height={"30vh"} src="https://github-readme-stats.vercel.app/api/top-langs/?username=Arnab-108&layout=compact" />
                     </Flex>
                         <Image id="github-streak-stats" width={"55vw"} height={"30vh"} src="https://github-readme-streak-stats.herokuapp.com/demo/preview.php?user=Arnab-108&theme=vue" />
